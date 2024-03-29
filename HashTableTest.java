f8952ef18b03abe81d950469c379bd010726e66a
isabelleacook1 <98196838+isabelleacook1@users.noreply.github.com>
2024-03-29 16:05:45 -0600

    Update HashTableTest.java

--- HashTableTest.java
+++ HashTableTest.java
@@ -1,1 +1,41 @@
+package assign09;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HashTableTest {
+	private HashTable<String, Integer> emptyHashStr;
+	private HashTable<String, Integer> hashStr;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		emptyHashStr = new HashTable<String, Integer>();
+		hashStr = new HashTable<String, Integer>();
+	}
+	
+	@Test
+	void testClearEmpty() {
+		assertEquals(emptyHashStr.size(), 0);
+		emptyHashStr.clear();
+		assertEquals(emptyHashStr.size(), 0);
+	}
+	
+	@Test
+	void testPutEmpty() {
+		assertNull(emptyHashStr.put("A", 1));
+	}
+	
+	@Test
+	void testIsEmpty() {
+		assertTrue(emptyHashStr.isEmpty());
+	}
+	
+	@Test
+	void testSizeEmpty() {
+		assertEquals(emptyHashStr.size(), 0);
+	}
+
+}
 
