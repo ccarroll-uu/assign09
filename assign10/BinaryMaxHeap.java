e00b4ec734b65d1da023943fc91d0317a4ce4b22
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-05 15:52:35 -0600

    Update BinaryMaxHeap.java

--- assign10/BinaryMaxHeap.java
+++ assign10/BinaryMaxHeap.java
@@ -8,31 +8,32 @@ import java.util.NoSuchElementException;
 public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 	private E[] arr;
 	private int size;
+	private Comparator<? super E> cmp;
 	
 	@SuppressWarnings("unchecked")
 	public BinaryMaxHeap() {
 		arr = (E[]) new Object[10];
 		size = 0;
-		// add inner compare?
+		innerCompare(null);
 	}
 	
 	@SuppressWarnings("unchecked")
 	public BinaryMaxHeap(Comparator<? super E> cmp) {
 		arr = (E[]) new Object[10];
 		size = 0;
-		// add inner compare?
+		innerCompare(cmp);
 	}
 
 	public BinaryMaxHeap(List<? extends E> list) {
 		buildHeap(list);
 		size = list.size();
-		// add inner compare?
+		innerCompare(cmp);
 	}
 	
 	public BinaryMaxHeap(List<? extends E> list, Comparator<? super E> cmp) {
 		buildHeap(list);
 		size = list.size();
-		// add inner compare?
+		innerCompare(cmp);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -50,11 +51,21 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 	}
 	
 	private void percolateDown(E[] arr, int index, int size) {
+		E temp = arr[size - 1];
+		arr[size - 1] = arr[0];
+		arr[0] = temp;
+		
 		
 	}
 	
-	private int innerCompare() {
-		
+	@SuppressWarnings("unchecked")
+	private void innerCompare(Comparator<? super E> cmp) {
+		if (cmp == null) {
+			this.cmp = (o1, o2) -> ((Comparable<? super E>)o1).compareTo(o2);
+		}
+		else {
+			this.cmp = cmp;	
+		}
 	}
 	
 	@Override
