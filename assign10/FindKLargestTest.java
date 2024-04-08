3bcd8ea2faa4f1bf161cc34b085315c906821c35
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-08 12:40:48 -0600

    Update FindKLargestTest.java

--- assign10/FindKLargestTest.java
+++ assign10/FindKLargestTest.java
@@ -9,10 +9,19 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * This class tests the methods from FindKLargest class.
+ * 
+ * @author Isabelle Cook and Courtney Carroll
+ * @version April 8, 2024
+ */
 class FindKLargestTest {
 	private ArrayList<String> list;
 	private ArrayList<String> listEmpty;
 	private Comparator<String> cmp;
+	private ArrayList<Integer> listInt;
+	private Comparator<Integer> cmp2;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		cmp = (str1, str2) -> ((Integer)str1.length()).compareTo(str2.length());
@@ -24,6 +33,16 @@ class FindKLargestTest {
 		list.add("dog");
 		list.add("elephant");
 		list.add("faeries");
+		
+		cmp2 = (int1, int2) -> ((int2.compareTo(int1)));
+		listInt = new ArrayList<Integer>();
+		listInt.add(3);
+		listInt.add(-2);
+		listInt.add(8);
+		listInt.add(0);
+		listInt.add(1);
+		listInt.add(-5);
+		listInt.add(10);
 	}
 
 	@Test
@@ -78,5 +97,62 @@ class FindKLargestTest {
 		assertEquals(kLargest.get(0), "elephant");
 		assertEquals(kLargest.get(1), "faeries");
 		assertEquals(kLargest.get(2), "banana");
+	}
+	
+	@Test
+	void testFindKLargestSort() {
+		List<String> kLargest = FindKLargest.findKLargestSort(list, 0);
+		assertTrue(kLargest.isEmpty());
+		
+		kLargest = FindKLargest.findKLargestSort(list, list.size());
+		assertEquals(kLargest.get(0), "faeries");
+		assertEquals(kLargest.get(1), "elephant");
+		assertEquals(kLargest.get(2), "dog");
+		assertEquals(kLargest.get(3), "cats");
+		assertEquals(kLargest.get(4), "banana");
+		assertEquals(kLargest.get(5), "apple");
+		
+		kLargest = FindKLargest.findKLargestSort(list, 3);
+		assertEquals(kLargest.get(0), "faeries");
+		assertEquals(kLargest.get(1), "elephant");
+		assertEquals(kLargest.get(2), "dog");
+	}
+	
+	@Test
+	void testFindKLargestSortCmp() {
+		List<String> kLargest = FindKLargest.findKLargestSort(list, 0, cmp);
+		assertTrue(kLargest.isEmpty());
+		
+		kLargest = FindKLargest.findKLargestSort(list, list.size(), cmp);
+		assertEquals(kLargest.get(0), "elephant");
+		assertEquals(kLargest.get(1), "faeries");
+		assertEquals(kLargest.get(2), "banana");
+		assertEquals(kLargest.get(3), "apple");
+		assertEquals(kLargest.get(4), "cats");
+		assertEquals(kLargest.get(5), "dog");
+		
+		kLargest = FindKLargest.findKLargestSort(list, 3, cmp);
+		assertEquals(kLargest.get(0), "elephant");
+		assertEquals(kLargest.get(1), "faeries");
+		assertEquals(kLargest.get(2), "banana");
+	}
+	@Test
+	void testFindKLargestHeapCmpInt() {
+		List<Integer> kLargest = FindKLargest.findKLargestHeap(listInt, 0, cmp2);
+		assertTrue(kLargest.isEmpty());
+		
+		kLargest = FindKLargest.findKLargestHeap(listInt, listInt.size(), cmp2);
+		assertEquals(kLargest.get(0), -5);
+		assertEquals(kLargest.get(1), -2);
+		assertEquals(kLargest.get(2), 0);
+		assertEquals(kLargest.get(3), 1);
+		assertEquals(kLargest.get(4), 3);
+		assertEquals(kLargest.get(5), 8);
+		assertEquals(kLargest.get(6), 10);
+		
+		kLargest = FindKLargest.findKLargestHeap(listInt, 3, cmp2);
+		assertEquals(kLargest.get(0), -5);
+		assertEquals(kLargest.get(1), -2);
+		assertEquals(kLargest.get(2), 0);
 	}
 }
