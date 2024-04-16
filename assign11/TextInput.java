92ef8e623976d95060a270aa22221764ffef05b0
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-15 20:50:06 -0600

    Update TextInput.java

--- assign11/TextInput.java
+++ assign11/TextInput.java
@@ -1,6 +1,7 @@
 package assign11;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -14,16 +15,27 @@ public class TextInput<T> {
 	}
 	
 	// Check Java LinkedList
-	private Hashtable<T, LinkedList<T>> readFromFile(File file){
+	private Hashtable<T, Hashtable<T, Integer>> readFromFile(File file){
 		Hashtable<T, Hashtable<T, Integer>> table = new Hashtable<T, Hashtable<T, Integer>>();
 		
 		try {
 			Scanner input = new Scanner(file);
+			
+			while (input.hasNext()) {
+				String word = input.next();
+				word.toLowerCase();
+				if (!Character.toString(word.charAt(0)).matches("[^\\w]")) {
+					String[] wordSplit = word.split("[^\\w]");
+					word = wordSplit[0];
+				}
+				
+			}
 		}
 		
-		catch {
-			
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
 		}
+		
+		
 	}
 }
-
