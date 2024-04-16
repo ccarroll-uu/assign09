f4157e7b78bdd99153200f2c9bb6f024dbe37635
isabelleacook1 <98196838+isabelleacook1@users.noreply.github.com>
2024-04-15 20:13:24 -0600

    Update TextInput.java

--- assign11/TextInput.java
+++ assign11/TextInput.java
@@ -1,1 +1,29 @@
+package assign11;
+
+import java.io.File;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class TextInput<T> {
+
+	private Hashtable<T, Hashtable<T, Integer>> table;
+	
+	public TextInput(File file) {
+		table = readFromFile(file);
+	}
+	
+	// Check Java LinkedList
+	private Hashtable<T, LinkedList<T>> readFromFile(File file){
+		Hashtable<T, Hashtable<T, Integer>> table = new Hashtable<T, Hashtable<T, Integer>>();
+		
+		try {
+			Scanner input = new Scanner(file);
+		}
+		
+		catch {
+			
+		}
+	}
+}
 
