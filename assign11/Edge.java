774628c69fe6afcab10ffdf9338faf635ead49d6
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-18 20:04:57 -0600

    Update Edge.java

--- assign11/Edge.java
+++ assign11/Edge.java
@@ -1,1 +1,27 @@
+package assign11;
 
+/**
+ * Represents an edge in a graph.
+ * 
+ * @author Erin Parker, Courtney Carroll
+ * @version March 12, 2024
+ */
+public class Edge<T> {
+	private Vertex<T> dest;
+	
+	/**
+	 * Constructs an edge.
+	 * @param dest - the vertex that is the destination of the edge.
+	 */
+	public Edge(Vertex<T> dest) {
+		this.dest = dest;
+	}
+	
+	/**
+	 * Gets the vertex that is the destination of the edge.
+	 * @return - vertex that is the destination of the edge.
+	 */
+	public Vertex<T> getDest(){
+		return this.dest;
+	}
+}
