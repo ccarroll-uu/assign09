3524bbc36c1a5ca5351786dc5567bd313d25856e
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-19 15:57:13 -0600

    Create TextGenerator.java

---
+++ assign11/TextGenerator.java
@@ -0,0 +1,27 @@
+package assign11;
+
+import java.io.File;
+
+public class TextGenerator {
+
+	public static void main(String[] args) {
+		String fileName = args[0];
+		String seedWord = args[1];
+		int k = Integer.parseInt(args[2]);
+		String outputType = null;
+		
+		if (args.length == 4) {
+			outputType = args[3];
+		}
+		
+		TextInput input = new TextInput(new File(fileName));
+		
+		if (outputType == null || outputType == "all") {
+			System.out.println(input.randomText(seedWord, k));
+		}
+		
+		
+
+	}
+
+}
