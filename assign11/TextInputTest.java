f858620c6c1f419ad7801938cb57fb6c7ba9216a
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-19 16:57:42 -0600

    Update TextInputTest.java

--- assign11/TextInputTest.java
+++ assign11/TextInputTest.java
@@ -15,21 +15,25 @@ class TextInputTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		str1 = "Baa, baa black sheep.txt";
-		str2 = "abc.txt";
-		str3 = "helloWorld.txt";
-		str4 = "trickyFormatting.txt";
+		str1 = "src/assign11/Baa, baa black sheep.txt";
+		str2 = "src/assign11/abc.txt";
+		str3 = "src/assign11/helloWorld.txt";
+		str4 = "src/assign11/trickyFormatting.txt";
 	}
 
 	@Test
 	void testStr3() {
 		TextInput input3 = new TextInput(new File(str3));
 		assertEquals(input3.randomText("hello", 0), "");
-		input3.randomText("hello", 2);
-		input3.randomText("hello", 4);
+		assertEquals(input3.randomText("hello", 2), "hello world");
+		assertEquals(input3.randomText("world", 2), "world world");
+		assertEquals(input3.randomText("hello", 4), "hello world hello world");
+		assertEquals(input3.randomText("world", 4), "world world world world");
+		
 		assertEquals(input3.mostLikelyText("hello", 0), "");
-		input3.mostLikelyText("helloWorld", 2);
-		input3.mostLikelyText("helloWorld", 4);
+		assertEquals(input3.mostLikelyText("hello", 2), "hello world");
+		assertEquals(input3.mostLikelyText("world", 2), "world world");
+		assertEquals(input3.mostLikelyText("hello", 4), "hello world hello world");
+		assertEquals(input3.mostLikelyText("world", 4), "world world world world");
 	}
 }
-
