53f6e1b4466b39149c6252c207136a7dc946fea5
isabelleacook1 <98196838+isabelleacook1@users.noreply.github.com>
2024-04-20 15:22:13 -0600

    Update TextInput.java

--- assign11/TextInput.java
+++ assign11/TextInput.java
@@ -43,10 +43,13 @@ public class TextInput{
 		try {
 			Scanner input = new Scanner(file);
 	
+			// Go through words in text file
 			while (input.hasNext()) {
 				String word = input.next();
+				// Check if word starts with invalid character
 				while (input.hasNext() && Character.toString(word.charAt(0)).matches("[^\\w]"))
 					word = input.next();
+				// Remove invalid characters
 				String[] wordSplit = word.split("[^\\w]");
 				word = wordSplit[0];
 				word = word.toLowerCase();
@@ -55,6 +58,7 @@ public class TextInput{
 					prev = word;
 				}
 				else {
+					// Check if word and word after are contained in table
 					if (table.containsKey(prev)) {
 						Hashtable<String, Integer> innerTable = table.get(prev);
 						if (innerTable.containsKey(word)) {
@@ -68,11 +72,13 @@ public class TextInput{
 						}
 					}
 					
+					// Add word and empty inner table to table
 					else {
 						Hashtable<String, Integer> innerTable = new Hashtable<String, Integer>();
 						innerTable.put(word, 1);
 						table.put(prev, innerTable);
 					}
+					
 					prev = word;
 				}
 			}
@@ -96,13 +102,17 @@ public class TextInput{
 	private Graph<String> makeGraph(Hashtable<String, Hashtable<String, Integer>> table) {
 		Graph<String> graph = new Graph<String>();
 		Enumeration<String> keys = table.keys();
+		// Go through all of words in table
 		for (int i = 0; i < table.size(); i++) {
 			String key = keys.nextElement();
 			Enumeration<String> innerKeys = table.get(key).keys();
+			// Go through all of words in inner table
 			for(int j = 0; j < table.get(key).size(); j++) {
 				String innerKey = innerKeys.nextElement();
+				// Calculate edge weight
 				int count = table.get(key).get(innerKey);
 				double weight = count / (double) table.get(key).size();
+				// Add edge with word in table going to word in inner table
 				graph.addEdge(key, innerKey, weight);
 			}
 		}
@@ -120,10 +130,11 @@ public class TextInput{
 		if (k == 0)
 			return "";
 		Random rng = new Random();
-		double number = rng.nextDouble(1);
+		
 		Vertex<String> v = graph.getVertex(seedWord);
 		String randomText = seedWord + " ";
 		for (int i = 1; i < k; i++) {
+			double number = rng.nextDouble(1);
 			if (v.getAdj().size() == 0) {
 				v = graph.getVertex(seedWord);
 				randomText += seedWord;
@@ -133,7 +144,7 @@ public class TextInput{
 				adjList.sort((o1, o2) -> o2.compare(o1, o2));
 				Iterator<Edge<String>> iter = adjList.iterator();
 				Edge<String> edge = iter.next();
-				double weightSum = 0;
+				double weightSum = edge.getWeight();
 				while (iter.hasNext() && weightSum < number) {
 					edge = iter.next();
 					weightSum += edge.getWeight();
