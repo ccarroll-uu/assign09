8dcf82c95147decf842c51d413921415ee4a3272
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-20 15:46:58 -0600

    Update TextGenerator.java

--- assign11/TextGenerator.java
+++ assign11/TextGenerator.java
@@ -1,8 +1,18 @@
-
 package assign11;
 
 import java.io.File;
 
+/**
+ * This class takes in 4 arguments from the command line. The first specifies the 
+ * input file name and the path to that file. The second specifies the seed word.
+ * The third specifies k, or how many words to generate from the text file. The fourth
+ * argument is optional; if it is given as "one", only the most probable text is generated.
+ * If it is "all" or the argument is not provided, it generates random words based on
+ * all probabilities.
+ * 
+ * @author Isabelle Cook and Courtney Carroll
+ * @version April 20, 2024
+ */
 public class TextGenerator {
 
 	public static void main(String[] args) {
