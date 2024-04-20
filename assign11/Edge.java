27a79c229e3cf450cee3bd61dd669e4d997a5818
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-20 15:46:12 -0600

    Update Edge.java

--- assign11/Edge.java
+++ assign11/Edge.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 /**
  * Represents an edge in a graph.
  * 
- * @author Erin Parker, Courtney Carroll
- * @version March 12, 2024
+ * @author Erin Parker, Courtney Carroll, Isabelle Cook
+ * @version April 20, 2024
  */
 public class Edge <T extends Comparable<T>> implements Comparator<Edge<T>>{
 	private Vertex<T> dest;
@@ -14,6 +14,7 @@ public class Edge <T extends Comparable<T>> implements Comparator<Edge<T>>{
 	
 	/**
 	 * Constructs an edge.
+	 * 
 	 * @param dest - the vertex that is the destination of the edge.
 	 */
 	public Edge(Vertex<T> dest, double weight) {
@@ -23,16 +24,30 @@ public class Edge <T extends Comparable<T>> implements Comparator<Edge<T>>{
 	
 	/**
 	 * Gets the vertex that is the destination of the edge.
+	 * 
 	 * @return - vertex that is the destination of the edge.
 	 */
 	public Vertex<T> getDest(){
 		return this.dest;
 	}
 	
+	/**
+	 * Returns edge weight.
+	 * 
+	 * @return edge weight
+	 */
 	public double getWeight() {
 		return this.weight;
 	}
 
+	/**
+	 * Compares two edges by weight, and then lexographically.
+	 * 
+	 * @param o1 - first edge
+	 * @param o2 - second edge
+	 * @return positive number if edge weight 1 > edge weight 2; negative number if
+	 * edge weight 2 > edge weight 1.
+	 */
 	@Override
 	public int compare(Edge<T> o1, Edge<T> o2) {
 		if (o1.weight - o2.weight == 0)
