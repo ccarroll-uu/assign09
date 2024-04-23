2d03a985e09d2e61df35cb638a1bbbdfc5f5cf3f
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-23 16:45:29 -0600

    Update TextInput.java

--- assign11/TextInput.java
+++ assign11/TextInput.java
@@ -1,4 +1,3 @@
-
 package comprehensive;
 
 import java.io.File;
@@ -6,64 +5,21 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * This class takes in a text file and generates random text similar to the text in
  * the file.
  * 
  * @author Isabelle Cook and Courtney Carroll
- * @version April 20, 2024
+ * @version April 23, 2024
  */
-public class TextInput {
-	private HashMap<String, ArrayList<Node>> table;
-	
-	/**
-	 * The private Node class creates nodes for word frequency.
-	 */
-	private class Node implements Comparator<Node>{
-		public String data;
-		public int frequency;
-		
-		/**
-		 * Creates a Node that stores data and frequency.
-		 * 
-		 * @param data - data in node
-		 * @param leftChild - left child node
-		 * @param rightChild - right child node
-		 * @param parent - parent node
-		 */
-		public Node(String data, int frequency) {
-			this.data = data;
-			this.frequency = frequency;
-		}
-
-		/**
-		 * Compares two Nodes, first by frequency and then by data.
-		 * 
-		 * @param o1 - first Node
-		 * @param o2 - second Node
-		 * @return positive number if first Node > second Node; negative number if second Node > first
-		 * Node; if first and second Node are equal, returns 0.
-		 */
-		@Override
-		public int compare(Node o1, Node o2) {
-			if (o1.frequency == o2.frequency) {
-				return o1.data.compareTo(o2.data);
-			}
-			return o2.frequency - o1.frequency;
-		}
-		
-		public boolean equals(Object o) {
-			if (!(o instanceof Node)) {
-	            return false;
-	        }
-			
-			Node other = (Node) o;
-			return this.data.equals(other.data);
-		}
-	}
+public class TextInput implements Comparator<Map.Entry<String, Integer>>{
+	private HashMap<String, HashMap<String, Integer>> table;
 	
 	/**
 	 * Constructor for the TextInput class.
@@ -79,10 +35,10 @@ public class TextInput {
 	 * Private method that stores frequency of words and their connections from text file.
 	 * 
 	 * @param file - given file
-	 * @return hash table storing the frequency of words their connections
+	 * @return hash map storing the frequency of words their connections
 	 */
-	private HashMap<String, ArrayList<Node>> readFromFile(File file){
-		HashMap<String, ArrayList<Node>> table = new HashMap<String, ArrayList<Node>>();
+	private HashMap<String, HashMap<String, Integer>> readFromFile(File file){
+		HashMap<String, HashMap<String, Integer>> table = new HashMap<String, HashMap<String, Integer>>();
 		String prev = null;
 		try {
 			Scanner input = new Scanner(file);
@@ -104,22 +60,23 @@ public class TextInput {
 				else {
 					// Check if word and word after are contained in table
 					if (table.containsKey(prev)) {
-						ArrayList<Node> nodeList = table.get(prev);
-						
-						if (nodeList.contains(new Node(word, 1))) {
-							int index = nodeList.indexOf(new Node(word, 1));
-							nodeList.get(index).frequency += 1;
+						HashMap<String, Integer> innerTable = table.get(prev);
+						if (innerTable.containsKey(word)) {
+							int count = innerTable.get(word);
+							innerTable.put(word, count++);
+							table.put(prev, innerTable);
 						}
 						else {
-							nodeList.add(new Node(word, 1));
+							innerTable.put(word, 1);
+							table.put(prev, innerTable);
 						}
 					}
 					
 					// Add word and empty inner table to table
 					else {
-						ArrayList<Node> nodeList = new ArrayList<Node>();
-						nodeList.add(new Node(word, 1));
-						table.put(prev, nodeList);
+						HashMap<String, Integer> innerTable = new HashMap<String, Integer>();
+						innerTable.put(word, 1);
+						table.put(prev, innerTable);
 					}
 					
 					prev = word;
@@ -148,35 +105,29 @@ public class TextInput {
 	public String kMostProbableWords(String seedWord, int k) {
 		if (k == 0)
 			return "";
-		ArrayList<Node> nodeList = table.get(seedWord);
+		HashMap<String, Integer> innerKeys = table.get(seedWord);
 		String text = "";
 		
 		// If inner key list is empty (no next word)
-		if (nodeList == null) {
+		if (innerKeys == null) {
 				return text;
 			}
 		
 		// Reverse sort inner key list
-		nodeList.sort((o1, o2) -> o1.compare(o1, o2));
+		Set<Entry<String, Integer>> innerKeySet = innerKeys.entrySet();
+		ArrayList<Entry<String, Integer>> innerKeyList = new ArrayList<Entry<String, Integer>>(innerKeySet);
+		innerKeyList.sort((Entry<String, Integer> o1, Entry<String, Integer> o2) -> compare(o1, o2));
 		
 		// Add k number of words
 		for (int i = 0; i < k; i++) {
-			if (i >= nodeList.size())
+			if (i >= innerKeyList.size())
 				break;
 			if (i > 0)
 				text += " ";
-			text += nodeList.get(i).data;	
+			text += innerKeyList.get(i).getKey();	
 		}
 		
 		return text;
-	}
-	
-	private int calcFrequency(ArrayList<Node> nodeList) {
-		int frequency = 0;
-		for(Node node: nodeList) {
-			frequency += node.frequency;
-		}
-		return frequency;
 	}
 	
 	/**
@@ -191,30 +142,30 @@ public class TextInput {
 			return "";
 		Random rng = new Random();
 		
-		ArrayList<Node> nodeList = table.get(seedWord);
+		HashMap<String, Integer> innerKeys = table.get(seedWord);
 		String randomText = seedWord;
-		
 		
 		// Add k number of words
 		for (int i = 1; i < k; i++) {
 			double number = rng.nextDouble(1);
 			// If inner key list is empty (no next word)
-			if (nodeList == null) {
-				nodeList = table.get(seedWord);
+			if (innerKeys == null) {
+				innerKeys = table.get(seedWord);
 				randomText += " " + seedWord;
 			}
 			// Reverse sort inner key list
 			else {
-				nodeList.sort((o1, o2) -> o1.compare(o1, o2));
-				int frequency = calcFrequency(nodeList);
-				
+				Set<Entry<String, Integer>> innerKeySet = innerKeys.entrySet();
+				ArrayList<Entry<String, Integer>> innerKeyList = new ArrayList<Entry<String, Integer>>(innerKeySet);
+				innerKeyList.sort((Entry<String, Integer> o1, Entry<String, Integer> o2) -> compare(o1, o2));
+
 				double weightSum = 0;
 				// Use random double to determine which word to generate
-				for (int j = 0; j < nodeList.size(); j++) {
-					weightSum += nodeList.get(j).frequency/ (double)frequency;
+				for (int j = 0; j < innerKeyList.size(); j++) {
+					weightSum += innerKeyList.get(j).getValue()/ (double) innerKeys.size();
 					if (weightSum > number) {
-						randomText += " " + nodeList.get(j).data;
-						nodeList = table.get(nodeList.get(j).data);
+						randomText += " " + innerKeyList.get(j).getKey();
+						innerKeys = table.get(innerKeyList.get(j).getKey());
 						break;
 					}	
 				}
@@ -237,24 +188,43 @@ public class TextInput {
 	public String mostLikelyText(String seedWord, int k) {
 		if (k == 0)
 			return "";
-		ArrayList<Node> nodeList = table.get(seedWord);
+		HashMap<String, Integer> innerKeys = table.get(seedWord);
 		String text = seedWord;
 		// Add k number of words
 		for (int i = 1; i < k; i++) {
 			// If inner key list is empty (no next word)
-			if (nodeList == null) {
-				nodeList = table.get(seedWord);
+			if (innerKeys == null) {
+				innerKeys = table.get(seedWord);
 				text += " " + seedWord;
 			}
 			// Sort list and add next word with highest frequency
 			else {
-				nodeList.sort((o1, o2) -> o1.compare(o1, o2));
-				text += " " + nodeList.get(0).data;
-				nodeList = table.get(nodeList.get(0).data);
+				Set<Entry<String, Integer>> innerKeySet = innerKeys.entrySet();
+				ArrayList<Entry<String, Integer>> innerKeyList = new ArrayList<Entry<String, Integer>>(innerKeySet);
+				innerKeyList.sort((Entry<String, Integer> o1, Entry<String, Integer> o2) -> compare(o1, o2));
+				
+				text += " " + innerKeyList.get(0).getKey();
+				innerKeys = table.get(innerKeyList.get(0).getKey());
 			}
 		}
 		return text;
 	}
 
+
+	/**
+	 * Compares two map entries, first by value and second by key.
+	 * 
+	 * @param o1 - first map entry
+	 * @param o2 - second map entry
+	 * @return positive number if first entry > second entry; negative number if second  entry > first
+	 * entry; if first and second entry are equal, returns 0.
+	 */
+	@Override
+	public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+		if (o1.getValue() == o2.getValue()) {
+			return o1.getKey().compareTo(o2.getKey());
+		}
+		return o2.getValue() - o1.getValue();
+	}
 	
 }
