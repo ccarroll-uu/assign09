23bbc1e1715071e9fa3edf7890857fee52f3c5cd
Courtney Carroll <78875820+ccarroll-uu@users.noreply.github.com>
2024-04-23 17:37:16 -0600

    Create TextGeneratorAnalysis.java

---
+++ assign11/TextGeneratorAnalysis.java
@@ -0,0 +1,68 @@
+package comprehensive;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+public class TextGeneratorAnalysis extends TimerTemplate {
+	
+	private final static int TIMES_TO_LOOP = 1000;   
+	private final static int NSTART = 20000;
+	private final static int NINCREMENT = 20000;
+	private final static int NSTEPS = 20;
+
+	public TextGeneratorAnalysis(int[] problemSizes, int timesToLoop) {
+		super(problemSizes, timesToLoop);
+	}
+	
+
+	@Override
+	protected void setup(int n) {
+		TextInput input = new TextInput(new File("src/comprehensive/CMC.txt"), n);
+	}
+
+	@Override
+	protected void timingIteration(int n) {
+		for (int i = 0; i < n; i++) {
+			input.kMostProbableWords()
+		}
+	}
+
+	@Override
+	protected void compensationIteration(int n) {
+		for (int i = 0; i < n; i++) {
+		}
+	}
+
+	public static void main(String[] args) {
+		DecimalFormat formatter = new DecimalFormat("00000E00");
+
+		System.out.println("\nN\t|  T(N)/1\tT(N)/logN\tT(N)/N\t\tT(N)/N^2\t\tT(N)/N^3\t\tT(N)/N^4");
+		System.out.println("-----------------------------------------------------------------------------------------------------------------");
+		
+		int[] problemSizes = new int[NSTEPS];
+		problemSizes[0] = NSTART;
+		for(int i = 1; i < NSTEPS; i++)
+			problemSizes[i] = problemSizes[i - 1] + NINCREMENT;
+		
+		TextGeneratorAnalysis checker = new TextGeneratorAnalysis(problemSizes, TIMES_TO_LOOP);
+		var results = checker.run();
+		
+		for(var result : results) {
+			int N = result.n();
+			double time = result.avgNanoSecs();
+			System.out.print(N + "\t|  ");
+			
+			System.out.println(formatter.format(time) + "\t" + 
+					formatter.format(time / (Math.log10(N) / Math.log10(2))) + "\t" + 
+					formatter.format(time / N) + "\t" + 
+					formatter.format((time / N) / N));
+					formatter.format(((time / N) / N) / N);
+					formatter.format((((time / N) / N) / N) / N);
+		}	
+		
+		for(var result : results) {
+			double time = result.avgNanoSecs();
+			System.out.print(time + ", ");
+		}	
+	}
+}
